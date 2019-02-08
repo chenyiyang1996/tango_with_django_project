bd46f6b14a5a251f84de503dcd6831915694c2d4
chenyi yang <2327605y@student.gla.ac.uk>
2019-02-08 01:49:05 +0000

    6th commit(5.6)

--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
-	
+	class Meta:
+		verbose_name_plural='Categories'
 	def _str_(self):
 		return self.name
 	
