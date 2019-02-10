41bc33beefe12232c6705083d19db53d70157439
chenyi yang <2327605y@student.gla.ac.uk>
2019-02-10 18:27:40 +0000

    chapter9-logout

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse 
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 def show_category(request, category_name_slug):
 	context_dict={}
@@ -181,3 +182,10 @@ def user_login(request):
 @login_required 
 def restricted(request): 
 	return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required 
+def user_logout(request): 
+	# Since we know the user is logged in, we can now just log them out. 
+	logout(request) 
+	# Take the user back to the homepage. 
+	return HttpResponseRedirect(reverse('index'))
