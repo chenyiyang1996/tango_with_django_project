4273ba1a885ba06deade7904e2a813d621d12f83
chenyi yang <2327605y@student.gla.ac.uk>
2019-02-11 15:36:50 +0000

    completed chapter3

--- rango/views.py
+++ rango/views.py
@@ -28,8 +28,8 @@ def index(request):
 	return render(request,'rango/index.html',context=context_dict)
 	
 def about(request):
-    about_context_dict = {'boldmessage': "About: This is a page about things; REAL things!"}
-    return render(request, 'rango/about.html', context=about_context_dict)
+	Html = "Rango says here is the about page <a href='/rango/'>Index</a><br />"
+	return HttpResponse(Html)
 	
 def add_category(request):
 
